e2867b260c7b3ec7588d9e7a1514fa55428af7d6
asimaranov <a.simaranov@gmail.com>
2025-03-09 15:29:13 +0300

    Migrated to server auth checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,12 @@
-'use client'
-import Link from 'next/link';
-import { useEffect } from 'react';
-import { useSession } from '~/server/auth/client';
-import { api } from '~/trpc/react';
-import Sidebar from './components/Sidebar/Sidebar';
-import { useRouter } from 'next/navigation';
+import { getServerSession } from '~/server/auth';
+import { redirect } from 'next/navigation'
 
-export default function Home() {
-  const session = useSession();
-  const router = useRouter();
+export default async function Home() {
+  const session = await getServerSession();
 
-  useEffect(() => {
-    if (session.data?.user) {
-      router.push('/dashboard');
-    }
-  }, [session.data?.user]);
-
-  return (
-    <div className="relative flex min-h-screen flex-row overflow-hidden bg-background">
-      <Sidebar />
-    </div>
-  );
+  if (session?.user) {
+    redirect('/dashboard');
+  } else {
+    redirect('/signin');
+  }
 }
