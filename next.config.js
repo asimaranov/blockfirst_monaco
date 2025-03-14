68512b296c8014580ad74b9b70d07ccc49989d02
asimaranov <a.simaranov@gmail.com>
2025-03-14 14:47:57 +0300

    Fixed i18next config

--- next.config.js
+++ next.config.js
@@ -3,14 +3,12 @@
  * for Docker builds.
  */
 import './src/env.js';
-import i18nConfig from './next-i18next.config.js';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import("next").NextConfig} */
 const config = {
-  i18n: i18nConfig.i18n,
 };
 
 export default withNextIntl(config);
