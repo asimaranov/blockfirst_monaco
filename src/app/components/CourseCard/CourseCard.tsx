fb21f3703c309573ea123fd84df8159537e983c3
asimaranov <a.simaranov@gmail.com>
2025-03-16 00:05:51 +0300

    Added soon labels'

--- src/app/components/CourseCard/CourseCard.tsx
+++ src/app/components/CourseCard/CourseCard.tsx
@@ -153,7 +153,7 @@ export function CourseCard({ course }: { course: ICourse }) {
           </div>
         ) : (
           <button
-            className={`flex h-11 w-full items-center justify-center gap-2 rounded-full`}
+            className={`flex h-11 w-full items-center justify-center gap-2 rounded-full z-100 bg-background`}
           >
             🔥Скоро🔥
           </button>
