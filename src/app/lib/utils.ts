fad993eb34d7f9b57459cb013e2f4dd5df0c1ee5
asimaranov <a.simaranov@gmail.com>
2025-03-21 17:22:42 +0300

    Fixed rubles

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,4 +1,4 @@
-export function formatPrice(price: number, currency: string = '') {
+export function formatPrice(price: number, currency: string = '₽') {
   const formatter = new Intl.NumberFormat('ru-RU');
   return `${currency}${formatter.format(Math.ceil(price))}`;
 }
