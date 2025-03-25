6f12fe30a6810af3b747004d5a56be5dcc915131
asimaranov <a.simaranov@gmail.com>
2025-03-25 10:34:37 +0300

    Fixes for icons

--- src/app/components/EmploymentPage/VacancyItem.tsx
+++ src/app/components/EmploymentPage/VacancyItem.tsx
@@ -96,6 +96,7 @@ export const VacancyItem = ({ vacancy, onApply }: VacancyItemProps) => {
                 viewBox="0 0 20 20"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
               >
                 <path
                   d="M5.44922 10.65L8.04922 13.25L14.5492 6.75"
