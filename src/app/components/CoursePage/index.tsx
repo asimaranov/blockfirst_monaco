128589ea6ebaf45b69ad6940e3a9616276700e86
asimaranov <a.simaranov@gmail.com>
2025-03-31 10:11:24 +0300

    Fixes

--- src/app/components/CoursePage/index.tsx
+++ src/app/components/CoursePage/index.tsx
@@ -324,8 +324,8 @@ const CourseSection = ({
       {section.lessons && (
         <div className="flex flex-col gap-4 px-5 sm:px-8 pt-5">
           {section.lessons.map((lesson, lessonIndex) => (
-            <div key={lessonIndex} className="flex items-center gap-1">
-              <div className="flex h-5 w-5 items-center justify-center">
+            <div key={lessonIndex} className="flex gap-1">
+              <div className="flex h-5 w-5 shrink-0 items-center justify-center">
                 <div className="h-1 w-1 rounded-full bg-[#9AA6B5] opacity-50" />
               </div>
               <span className="text-sm text-gray-100">
