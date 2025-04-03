4924513f24c1e6df77e532715bc9ba889a91d9ef
asimaranov <a.simaranov@gmail.com>
2025-04-03 11:57:16 +0300

    Removed seesion cache

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,25 +1,12 @@
-import { betterAuth, type BetterAuthOptions } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { openAPI, admin } from "better-auth/plugins";
 import { headers } from "next/headers";
-import { cache } from "react";
-import { env } from "~/env";
-import {
-  sendChangeEmailVerification,
-  sendResetPasswordEmail,
-  sendVerificationEmail,
-} from "~/server/auth/email";
-import { PrismaClient } from "@prisma/client";
 import { auth } from "./config";
 
-const prisma = new PrismaClient();
-
-export const getServerSession = cache(
-  async () =>
-    await auth.api.getSession({
-      headers: await headers(),
-    }),
-);
+export const getServerSession = async () => {
+  "use server";
+  await auth.api.getSession({
+    headers: await headers(),
+  });
+}
 
 export { auth };
 export type Session = typeof auth.$Infer.Session;
