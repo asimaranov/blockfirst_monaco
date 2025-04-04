d53bbc4c3bb8ab4ecaa7a3923059a5268d32acb5
asimaranov <a.simaranov@gmail.com>
2025-04-04 22:10:02 +0300

    Fixed build error

--- src/app/components/DiplomaPage/QrCode.tsx
+++ src/app/components/DiplomaPage/QrCode.tsx
@@ -3,55 +3,60 @@
 import React, { useEffect, useRef, useState } from 'react';
 import QRCodeStyling from 'qr-code-styling';
 
-const qrCode = new QRCodeStyling({
-  type: 'svg',
-  width: 300,
-  height: 300,
-  margin: 10,
-  // image:
-  //   '/images/misc/qr-logo.svg',
-  dotsOptions: {
-    color: '#01050D',
-    type: 'rounded',
-    gradient: {
-      type: 'linear',
-      rotation: 90,
-      colorStops: [
-        { offset: 0, color: '#01050D' },
-        { offset: 1, color: '#01050D' },
-      ],
-    },
-  },
-  backgroundOptions: {
-    color: '#f2f2f2',
-    round: 0.2,
-  },
-  cornersSquareOptions: {
-    type: 'rounded',
-  },
-  cornersDotOptions: {
-    type: 'rounded',
-  },
-  imageOptions: {
-    crossOrigin: 'anonymous',
-    margin: 10,
-  },
-});
-
 export default function QrCode({ data }: { data: string }) {
   const ref = useRef<HTMLDivElement>(null);
+  const [qrCode, setQrCode] = useState<QRCodeStyling>();
 
   useEffect(() => {
+    const qrCode = new QRCodeStyling({
+      type: 'svg',
+      width: 300,
+      height: 300,
+      margin: 10,
+      // image:
+      //   '/images/misc/qr-logo.svg',
+      dotsOptions: {
+        color: '#01050D',
+        type: 'rounded',
+        gradient: {
+          type: 'linear',
+          rotation: 90,
+          colorStops: [
+            { offset: 0, color: '#01050D' },
+            { offset: 1, color: '#01050D' },
+          ],
+        },
+      },
+      backgroundOptions: {
+        color: '#f2f2f2',
+        round: 0.2,
+      },
+      cornersSquareOptions: {
+        type: 'rounded',
+      },
+      cornersDotOptions: {
+        type: 'rounded',
+      },
+      imageOptions: {
+        crossOrigin: 'anonymous',
+        margin: 10,
+      },
+    });
+
     if (ref.current) {
       qrCode.append(ref.current);
     }
+
+    setQrCode(qrCode);
   }, []);
 
   useEffect(() => {
-    qrCode.update({
-      data: data,
-    });
-  }, [data]);
+    if (qrCode) {
+      qrCode.update({
+        data: data,
+      });
+    }
+  }, [data, qrCode]);
 
   return (
     <div
