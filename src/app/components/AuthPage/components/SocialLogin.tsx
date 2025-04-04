c9e45f16671e51013fcf8d9c25c9c66dcc96636a
asimaranov <a.simaranov@gmail.com>
2025-04-04 19:14:02 +0300

    Loading icons for socual auth

--- src/app/components/AuthPage/components/SocialLogin.tsx
+++ src/app/components/AuthPage/components/SocialLogin.tsx
@@ -1,13 +1,62 @@
 import { authClient } from '~/server/auth/client';
 import VkLoginIcon from '../assets/social/vk';
 import GoogleLoginIcon from '../assets/social/google';
+import { useEffect, useState } from 'react';
+import LoadingIcon from '../assets/social/loading.svg';
+import Image from 'next/image';
+
+const SocialIcon = ({
+  icon,
+  onClick,
+}: {
+  icon: React.ReactNode;
+  onClick: () => Promise<void>;
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  return (
+    <button
+      className="flex cursor-pointer items-center justify-center"
+      onClick={async () => {
+        setIsLoading(true);
+
+        try {
+          await onClick();
+        } catch (error) {
+          console.error('Error in social login', error);
+          setIsLoading(false);
+        }
+      }}
+    >
+      {isLoading ? (
+        <Image
+          src={LoadingIcon}
+          alt="Loading"
+          className="h-11 w-11 shrink-0 animate-spin sm:h-13 sm:w-13"
+        />
+      ) : (
+        icon
+      )}
+    </button>
+  );
+};
 
 export default function SocialLogin() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 5000);
+  // }, []);
+
   return (
     <div className="mt-10 flex w-full items-center justify-center gap-3">
-      <button
-        className="flex items-center justify-center cursor-pointer"
+      <SocialIcon
+        icon={<GoogleLoginIcon />}
         onClick={async () => {
+          setIsLoading(true);
           try {
             await authClient.signIn.social({
               provider: 'google',
@@ -18,20 +67,23 @@ export default function SocialLogin() {
             alert('Error in google signin');
           }
         }}
-      >
-        <GoogleLoginIcon />
-      </button>
-      <button
-        className="flex items-center justify-center cursor-pointer"
+      />
+
+      <SocialIcon
+        icon={<VkLoginIcon />}
         onClick={async () => {
-          await authClient.signIn.social({
-            provider: 'vk',
-            callbackURL: '/dashboard',
-          });
+          setIsLoading(true);
+          try {
+            await authClient.signIn.social({
+              provider: 'vk',
+              callbackURL: '/dashboard',
+            });
+          } catch (error) {
+            console.error('Error in google signin', error);
+            alert('Error in google signin');
+          }
         }}
-      >
-        <VkLoginIcon />
-      </button>
+      />
     </div>
   );
 }
