ea36c1d483033e8481f31cc5fcd5934a3ce5aa29
asimaranov <a.simaranov@gmail.com>
2025-04-04 19:26:46 +0300

    Fixed topbar

--- src/app/components/CVPage/Topbar.tsx
+++ src/app/components/CVPage/Topbar.tsx
@@ -10,7 +10,7 @@ export function Topbar({ lastestUpdate }: { lastestUpdate: string }) {
       showBorder={false}
       className="relative bg-[#01050d] bg-[url('/images/misc/cv-header-bg-mobile.png')] sm:bg-[url('/images/misc/cv-header-bg.png')] bg-cover bg-center"
       leftContent={
-        <div className="flex flex-row items-center space-x-4 sm:space-x-5">
+        <div className="flex flex-row space-x-4 sm:space-x-5">
           <div className="relative h-10 w-10 sm:h-15.25 sm:w-15.25 overflow-hidden shrink-0">
             {/* Logo/Icon container with images from Figma */}
             <div className="absolute inset-0 bg-[#01050d]">
