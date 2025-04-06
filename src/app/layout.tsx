4a0e77597405cfc31f3d5723781f0e3bf7fa6fbe
asimaranov <a.simaranov@gmail.com>
2025-04-06 22:43:27 +0300

    Enabled fonts optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '~/styles/globals.css';
 
 import { type Metadata } from 'next';
+import { lato, roboto, delight } from './fonts';
 
 import { TRPCReactProvider } from '~/trpc/react';
 import { NextIntlClientProvider } from 'next-intl';
@@ -8,10 +9,9 @@ import { getLocale } from 'next-intl/server';
 
 export const metadata: Metadata = {
   title: 'Blockfirst - образовательная блокчейн платформа',
-  description: 'Начните свой путь в web3 с помощью специально разработанной платформы и персонального AI-ментора ',
-  icons: [
-    { rel: 'icon', type: 'image/png', url: '/icon.png' },
-  ],
+  description:
+    'Начните свой путь в web3 с помощью специально разработанной платформы и персонального AI-ментора ',
+  icons: [{ rel: 'icon', type: 'image/png', url: '/icon.png' }],
 };
 
 export default async function RootLayout({
@@ -20,7 +20,10 @@ export default async function RootLayout({
   const locale = await getLocale();
 
   return (
-    <html lang={locale}>
+    <html
+      lang={locale}
+      className={`${lato.variable} ${roboto.variable} ${delight.variable}`}
+    >
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="noindex, nofollow" />
