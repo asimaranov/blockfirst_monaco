1eee6123e9d867b5daef28df5c808efc5ac7055b
asimaranov <a.simaranov@gmail.com>
2025-05-02 13:59:02 +0300

    Fixes

--- src/app/components/TaskView/AiMentor.tsx
+++ src/app/components/TaskView/AiMentor.tsx
@@ -131,7 +131,7 @@ export default function AiMentor({ task }: { task: any }) {
                   className="h-9 w-9"
                 />
 
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 max-w-121">
                   <div className="px-4 py-2 pr-8">
                     <AiMessage richText={(x as any).md} />
                   </div>
