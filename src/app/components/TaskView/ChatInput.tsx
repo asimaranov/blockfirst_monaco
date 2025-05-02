aecbdff89c41c8520b0262422fafb40829148440
asimaranov <a.simaranov@gmail.com>
2025-05-02 17:03:12 +0300

    Removed shift enter

--- src/app/components/TaskView/ChatInput.tsx
+++ src/app/components/TaskView/ChatInput.tsx
@@ -33,7 +33,7 @@ export default function ChatInput({ onSendMessage, isGenerating }: ChatInputProp
       <textarea
         ref={textareaRef}
         className="placeholder:text-secondary flex w-full resize-none items-center justify-center text-sm outline-hidden"
-        placeholder={`Введите сообщение.\nShift + Enter для переноса строки`}
+        placeholder={`Введите сообщение.`}
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         onInput={(e) => {
