430c41ad6df3ea5ea39b3a2c83c135daecdcc01a
asimaranov <a.simaranov@gmail.com>
2025-05-31 22:27:39 +0300

    Custom trpc url

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -73,6 +73,7 @@ export function TRPCReactProvider(props: { children: ReactNode }) {
 }
 
 function getBaseUrl() {
+  if (process.env.TRPC_URL) return process.env.TRPC_URL;
   if (typeof window !== "undefined") return window.location.origin;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
